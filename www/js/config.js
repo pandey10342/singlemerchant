be10faaa2cbb1d53114627b91e4a6e965aaa1e13
unknown <rohitp@techepoch.com>
2020-04-19 21:27:06 +0530

    sdf

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl':"http://appetics.online/singlemerchant/api",			
+	'DialogDefaultTitle':"Appetics",			
+	'MerchantKeys':"c20cd81e12a5dd2c0e9a0c23713bc359",
+	'debug': false
+};
