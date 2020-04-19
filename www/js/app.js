ca8b20b98c6704a3002476e20db36d55968a8935
unknown <rohitp@techepoch.com>
2020-04-19 21:53:22 +0530

    ahggd

--- www/js/app.js
+++ www/js/app.js
@@ -63,6 +63,7 @@ var empty = function(data) {
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
+	document.removeEventListener('deviceready', onDeviceReady, false);
 	   admob.setOptions({
         publisherId:          "ca-app-pub-6871330764548204~5867465548",  // Required
     });
